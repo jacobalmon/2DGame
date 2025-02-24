2ea0a02908bd537e0eb112b15b94f5f384e7bc40
Oscar Lopez <53553460+oscar-dev19@users.noreply.github.com>
2025-02-24 09:25:14 -0800

    added brown floor.

--- 2dgame.cpp
+++ 2dgame.cpp
@@ -4,14 +4,14 @@
 #include "Goblin.h"
 
 int main() {
-    InitWindow(800, 600, "2D  Game");
+    InitWindow(800, 600, "2D Game");
 
-    // Creating Samurai.
-    Samurai samurai((Vector2) {400, 300});
+    // Creating Samurai at (300, 450) on the same level
+    Samurai samurai((Vector2) {300, 450});
     samurai.loadTextures();
 
-    // Creating Goblin.
-    Goblin goblin((Vector2) {400, 300});
+    // Creating Goblin at (500, 450) on the same level
+    Goblin goblin((Vector2) {500, 450});
     goblin.loadTextures();
 
     // Set FPS at 60.
@@ -30,11 +30,17 @@ int main() {
         // Drawing.
         BeginDrawing();
         ClearBackground(GREEN);
+
+        // Draw ground/platform
+        DrawRectangle(0, 500, 800, 100, DARKBROWN);
+        
         samurai.draw();
         goblin.draw();
+        
         EndDrawing();
     }
 
     CloseWindow();
     return 0;
 }
+
