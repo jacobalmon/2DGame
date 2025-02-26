9a688b55c53ff2a657407c3091825a2308439f22
Oscar Lopez <53553460+oscar-dev19@users.noreply.github.com>
2025-02-26 15:36:04 -0800

    Revert "reset all and matched all functionality as main branch did not rebase yet until all my code works and is approved."
    This reverts commit 3cdd0a3ef810f605ea2e5e6064978a0201771ff1.

--- 2dgame.cpp
+++ 2dgame.cpp
@@ -2,31 +2,17 @@
 #include <iostream>
 #include "Samurai.h"
 #include "Goblin.h"
-#include "Werewolf.h"
-#include "Wizard.h"
-#include "Demon.h"
 
 int main() {
-    InitWindow(800, 600, "2D  Game");
+    InitWindow(800, 600, "2D Game");
 
-    // Creating Samurai.
-    Samurai samurai((Vector2) {400, 300});
+    // Creating Samurai at (300, 450) on the same level
+    Samurai samurai((Vector2) {300, 450});
     samurai.loadTextures();
 
-    // Creating Goblin.
-    Goblin goblin((Vector2) {400, 300});
+    // Creating Goblin at (500, 450) on the same level
+    Goblin goblin((Vector2) {500, 450});
     goblin.loadTextures();
-
-    // Creating Werewolf.
-    Werewolf werewolf((Vector2 {400, 300}));
-    werewolf.loadTextures();
-
-    // Creating Wizard.
-    Wizard wizard((Vector2 {400, 300}));
-    wizard.loadTextures();
-
-    // Creating Demon.
-    Demon demon((Vector2 {400, 300}));
 
     // Set FPS at 60.
     SetTargetFPS(60);
@@ -41,29 +27,20 @@ int main() {
         goblin.applyVelocity();
         goblin.updateAnimation();
 
-        werewolf.move();
-        werewolf.applyVelocity();
-        werewolf.updateAnimation();
-
-        wizard.move();
-        wizard.applyVelocity();
-        wizard.updateAnimation();
-
-        demon.move();
-        demon.applyVelocity();
-        demon.updateAnimation();
-
         // Drawing.
         BeginDrawing();
         ClearBackground(GREEN);
+
+        // Draw ground/platform
+        DrawRectangle(0, 500, 800, 100, DARKBROWN);
+        
         samurai.draw();
         goblin.draw();
-        werewolf.draw();
-        wizard.draw();
-        demon.draw();
+        
         EndDrawing();
     }
 
     CloseWindow();
     return 0;
 }
+
